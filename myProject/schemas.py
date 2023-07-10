b775ca0677abb036e84f8ff07b1fc12b8f0c3f12
Ruben Pinxten <91123119+rubenpinxten@users.noreply.github.com>
2023-07-10 04:16:36 +0200

    testen of alles werkt

--- myProject/schemas.py
+++ myProject/schemas.py
@@ -4,7 +4,16 @@ class QuoteBase(BaseModel):
     text: str
 
 class QuoteCreate(QuoteBase):
-    pass
+    name_id: int
+    periode_id: int
+
+class Quote(QuoteBase):
+    id: int
+    name_id: int
+    periode_id: int
+
+    class Config:
+        orm_mode = True
 
 class TitleBase(BaseModel):
     text: str
@@ -12,11 +21,25 @@ class TitleBase(BaseModel):
 class TitleCreate(TitleBase):
     pass
 
+class Title(TitleBase):
+    id: int
+    quotes: list[Quote] = []
+
+    class Config:
+        orm_mode = True
+
 class YearBase(BaseModel):
     text: str
 
 class YearCreate(YearBase):
     pass
+
+class Year(YearBase):
+    id: int
+    spoken: list[Quote] = []
+
+    class Config:
+        orm_mode = True
 
 class QuoteCreateData(BaseModel):
     quote: QuoteCreate
