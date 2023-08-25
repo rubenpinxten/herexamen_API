c53affa01a9ec342e7f8d512bafccaeda9a8c66b
Ruben Pinxten <91123119+rubenpinxten@users.noreply.github.com>
2023-08-25 07:25:40 +0200

    test

--- myProject/crud.py
+++ myProject/crud.py
@@ -1,10 +1,8 @@
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-
 from models import Quote, Title, Year
 from schemas import QuoteBase, QuoteCreate, TitleBase, TitleCreate, YearBase, YearCreate
-
-
+import random
 import auth
 import models
 import schemas
@@ -36,6 +34,9 @@ def create_quote(db: Session, quote: QuoteCreate, title_text: str, year_text: st
 def get_quote(db: Session, quote_id: int):
     return db.query(Quote).filter(Quote.id == quote_id).first()
 
+def get_quote_random(db:Session, quote_id:int):
+    return db.query(Quote).filter(Quote.id == random.randint(1, 10))
+
 def update_quote(db: Session, quote_id: int, quote: QuoteBase):
     db_quote = db.query(Quote).filter(Quote.id == quote_id).first()
     db_quote.text = quote.text
@@ -49,22 +50,8 @@ def delete_quote(db: Session, quote_id: int):
     db.commit()
     return {"message": "Quote deleted"}
 
-def create_title(db: Session, title: TitleCreate):
-    db_title = Title(text=title.text)
-    db.add(db_title)
-    db.commit()
-    db.refresh(db_title)
-    return db_title
-
 def get_title(db: Session, title_id: int):
     return db.query(Title).filter(Title.id == title_id).first()
-
-def update_title(db: Session, title_id: int, title: TitleBase):
-    db_title = db.query(Title).filter(Title.id == title_id).first()
-    db_title.text = title.text
-    db.commit()
-    db.refresh(db_title)
-    return db_title
 
 def delete_title(db: Session, title_id: int):
     db_title = db.query(Title).filter(Title.id == title_id).first()
@@ -72,22 +59,8 @@ def delete_title(db: Session, title_id: int):
     db.commit()
     return {"message": "Title deleted"}
 
-def create_year(db: Session, year: YearCreate):
-    db_year = Year(text=year.text)
-    db.add(db_year)
-    db.commit()
-    db.refresh(db_year)
-    return db_year
-
 def get_year(db: Session, year_id: int):
     return db.query(Year).filter(Year.id == year_id).first()
-
-def update_year(db: Session, year_id: int, year: YearBase):
-    db_year = db.query(Year).filter(Year.id == year_id).first()
-    db_year.text = year.text
-    db.commit()
-    db.refresh(db_year)
-    return db_year
 
 def delete_year(db: Session, year_id: int):
     db_year = db.query(Year).filter(Year.id == year_id).first()
