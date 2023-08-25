85642e1b3d1f138a1cd5ccbcaf0e8fd937924332
Ruben Pinxten <91123119+rubenpinxten@users.noreply.github.com>
2023-08-25 09:27:32 +0200

    aanpassingen

--- myProject/main.py
+++ myProject/main.py
@@ -64,7 +64,7 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
     #Return the JWT as a bearer token to be placed in the headers
     return {"access_token": access_token, "token_type": "bearer"}
 
-
+#create quote with title and year
 @app.post("/quotes/", response_model=schemas.Quote)
 def create_quote(quote_data: schemas.QuoteCreateData, db: Session = Depends(get_db)):
     quote = quote_data.quote
@@ -74,6 +74,7 @@ def create_quote(quote_data: schemas.QuoteCreateData, db: Session = Depends(get_
     db_quote = crud.create_quote(db=db, quote=quote, title_text=title_text, year_text=year_text)
     return db_quote
 
+#get quote by id
 @app.get("/quotes/{quote_id}", response_model=schemas.Quote)
 def read_quote(quote_id: int, db: Session = Depends(get_db)):
     db_quote = crud.get_quote(db=db, quote_id=quote_id)
@@ -81,6 +82,15 @@ def read_quote(quote_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Quote not found")
     return db_quote
 
+#get random quote
+@app.get("/quotes/random", response_model=schemas.Quote)
+def read_quote(db: Session = Depends(get_db)):
+    db_quote = crud.get_quote_random(db=db)
+    if db_quote is None:
+        raise HTTPException(status_code=404, detail="Quote not found")
+    return db_quote
+
+#change quote by id
 @app.put("/quotes/{quote_id}", response_model=schemas.Quote)
 def update_quote(quote_id: int, quote: schemas.QuoteBase, db: Session = Depends(get_db)):
     db_quote = crud.get_quote(db=db, quote_id=quote_id)
@@ -88,8 +98,9 @@ def update_quote(quote_id: int, quote: schemas.QuoteBase, db: Session = Depends(
         raise HTTPException(status_code=404, detail="Quote not found")
     return crud.update_quote(db=db, quote_id=quote_id, quote=quote)
 
+#delete quote by id
 @app.delete("/quotes/{quote_id}")
-def delete_quote(quote_id: int, db: Session = Depends(get_db)):
+def delete_quote(quote_id: int, db: Session = Depends(get_db), token: str = Depends(auth.oauth2_scheme)):
     db_quote = crud.get_quote(db=db, quote_id=quote_id)
     if db_quote is None:
         raise HTTPException(status_code=404, detail="Quote not found")
@@ -97,10 +108,7 @@ def delete_quote(quote_id: int, db: Session = Depends(get_db)):
 
 # Title endpoints
 
-@app.post("/titles/", response_model=schemas.Title)
-def create_title(title: schemas.TitleCreate, db: Session = Depends(get_db)):
-    return crud.create_title(db=db, title=title)
-
+#get title by id
 @app.get("/titles/{title_id}", response_model=schemas.Title)
 def read_title(title_id: int, db: Session = Depends(get_db)):
     db_title = crud.get_title(db=db, title_id=title_id)
@@ -108,15 +116,9 @@ def read_title(title_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Title not found")
     return db_title
 
-@app.put("/titles/{title_id}", response_model=schemas.Title)
-def update_title(title_id: int, title: schemas.TitleBase, db: Session = Depends(get_db)):
-    db_title = crud.get_title(db=db, title_id=title_id)
-    if db_title is None:
-        raise HTTPException(status_code=404, detail="Title not found")
-    return crud.update_title(db=db, title_id=title_id, title=title)
-
+#delete title by id
 @app.delete("/titles/{title_id}")
-def delete_title(title_id: int, db: Session = Depends(get_db)):
+def delete_title(title_id: int, db: Session = Depends(get_db), token: str = Depends(auth.oauth2_scheme)):
     db_title = crud.get_title(db=db, title_id=title_id)
     if db_title is None:
         raise HTTPException(status_code=404, detail="Title not found")
@@ -124,10 +126,7 @@ def delete_title(title_id: int, db: Session = Depends(get_db)):
 
 # Year endpoints
 
-@app.post("/years/", response_model=schemas.Year)
-def create_year(year: schemas.YearCreate, db: Session = Depends(get_db)):
-    return crud.create_year(db=db, year=year)
-
+#get year by id
 @app.get("/years/{year_id}", response_model=schemas.Year)
 def read_year(year_id: int, db: Session = Depends(get_db)):
     db_year = crud.get_year(db=db, year_id=year_id)
@@ -135,29 +134,26 @@ def read_year(year_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Year not found")
     return db_year
 
-@app.put("/years/{year_id}", response_model=schemas.Year)
-def update_year(year_id: int, year: schemas.YearBase, db: Session = Depends(get_db)):
-    db_year = crud.get_year(db=db, year_id=year_id)
-    if db_year is None:
-        raise HTTPException(status_code=404, detail="Year not found")
-    return crud.update_year(db=db, year_id=year_id, year=year)
-
+#delete year by id
 @app.delete("/years/{year_id}")
-def delete_year(year_id: int, db: Session = Depends(get_db)):
+def delete_year(year_id: int, db: Session = Depends(get_db), token: str = Depends(auth.oauth2_scheme)):
     db_year = crud.get_year(db=db, year_id=year_id)
     if db_year is None:
         raise HTTPException(status_code=404, detail="Year not found")
     return crud.delete_year(db=db, year_id=year_id)
 
+#get all quotes
 @app.get("/quotes/all", response_model=list[schemas.Quote])
 def get_all_quotes(db: Session = Depends(get_db)):
     return crud.get_all_quotes(db=db)
 
-@app.get("/titles/get/", response_model=list[schemas.Title])
+#get all titles
+@app.get("/titles/all", response_model=list[schemas.Title])
 def get_all_titles(db: Session = Depends(get_db)):
     return crud.get_all_titles(db=db)
 
-@app.get("/years/get/", response_model=list[schemas.Year])
+#get all years
+@app.get("/years/all", response_model=list[schemas.Year])
 def get_all_years(db: Session = Depends(get_db)):
     return crud.get_all_years(db=db)
 
@@ -183,7 +179,7 @@ def read_admin(username: str, db: Session = Depends(get_db)):
 
 # DELETE admin
 @app.delete("/admin/{username}", response_model=str)
-async def delete_admin(username: str, db: Session = Depends(get_db)):
+async def delete_admin(username: str, db: Session = Depends(get_db), token: str = Depends(auth.oauth2_scheme)):
     admin_username = crud.get_admin_username(db, username)
     deleted_admin = crud.delete_admin(db, admin_username)
     return "Admin: " + str(deleted_admin.username) + " deleted"
